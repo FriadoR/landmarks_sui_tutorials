867f9020fd5afab91408b0ff040114a8d64d71be
Sachenko Sergey <braun.krieg@googlemail.com>
2025-01-29 15:02:26 +0300

    add favourite button/new folders

---
+++ Landmarks/Views/FavoriteButton.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Сергей Саченко on 29.01.2025.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteButton()
+}
