8a2248d4723e62e82ac9b01e4477051a40cd4925
Sachenko Sergey <braun.krieg@googlemail.com>
2025-02-02 23:26:23 +0300

    set changes

--- Landmarks/Views/Hikes/HikeGraph.swift
+++ Landmarks/Views/Hikes/HikeGraph.swift
@@ -1,10 +1,3 @@
-/*
-See the LICENSE.txt file for this sample’s licensing information.
-
-Abstract:
-The elevation, heart rate, and pace of a hike plotted on a graph.
-*/
-
 import SwiftUI
 
 extension Animation {
