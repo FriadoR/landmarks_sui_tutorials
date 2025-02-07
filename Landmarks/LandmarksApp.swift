2934f07950643f395e94e19312bd1d7a6234b887
Sachenko Sergey <braun.krieg@googlemail.com>
2025-02-07 17:37:06 +0300

    update toggle/update theme/use dispatch async

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -7,15 +7,34 @@
 
 import SwiftUI
 
-// MARK: -  class of obs
+// MARK: -  class of obs theme
 class ThemeManager: ObservableObject {
-    @Published var isDarkMode: Bool {
-        didSet {
-            UserDefaults.standard.set(isDarkMode, forKey: "isDarkMode")
+    
+    @AppStorage("isDarkMode") var isDarkMode: Bool = false
+    
+//    @Published var isDarkMode: Bool {
+//        didSet {
+//            UserDefaults.standard.set(isDarkMode, forKey: "isDarkMode")
+//            updateUITheme(isDarkMode: isDarkMode)
+//        }
+//    }
+//    
+//    init () {
+//        self.isDarkMode = UserDefaults.standard.bool(forKey: "isDarkMode")
+//    }
+// function for change theme
+    
+    func updateUITheme() {
+        DispatchQueue.main.async {
+            guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+                  let window = windowScene.windows.first else { return }
+            
+            window.overrideUserInterfaceStyle = self.isDarkMode ? .dark : .light
         }
     }
-    init () {
-        self.isDarkMode = UserDefaults.standard.bool(forKey: "isDarkMode")
+    func toggleTheme() {
+        isDarkMode.toggle()
+        updateUITheme()
     }
 }
 
@@ -24,7 +43,7 @@ struct LandmarksApp: App {
     @StateObject private var themeManager = ThemeManager()
     
     // Add dark/light theme
-    @AppStorage("isDarkMode") private var isDarkMode: Bool = false
+//    @AppStorage("isDarkMode") private var isDarkMode: Bool = false
     
     @State private var modelData = ModelData()
     
@@ -33,7 +52,7 @@ struct LandmarksApp: App {
             ContentView()
                 .environment(modelData)
                 .environmentObject(themeManager)
-                .preferredColorScheme(isDarkMode ? .dark : .light)
+                .preferredColorScheme(themeManager.isDarkMode ? .dark : .light)
         }
     }
 }
--- Landmarks/Views/Profiles/ProfileSummary.swift
+++ Landmarks/Views/Profiles/ProfileSummary.swift
@@ -9,10 +9,11 @@ import SwiftUI
 
 struct ProfileSummary: View {
     
-    @AppStorage("isDarkMode") private var isDarkMode = false
-    @EnvironmentObject var ThemeManager: ThemeManager
+//    @AppStorage("isDarkMode") private var isDarkMode = false
+    @EnvironmentObject var themeManager: ThemeManager
     @Environment(ModelData.self) var modelData
     var profile: Profile
+    
     
     var body: some View {
         ScrollView {
@@ -24,14 +25,14 @@ struct ProfileSummary: View {
                 Text("Уведомления: \(profile.prefersNotifications ? "On": "Off" )")
                 Text("Сезонный стикер: \(profile.seasonalPhoto.rawValue)")
                 Text("Дата посещения: ") + Text(profile.goalDate, style: .date)
-                Toggle("Использовать тёмную тему", isOn: $ThemeManager.isDarkMode)
-                    .onChange(of: ThemeManager.isDarkMode) { newValue in
-                            withAnimation {
-                                UIApplication.shared.windows.first?.rootViewController?.view.overrideUserInterfaceStyle = ThemeManager.isDarkMode ? .dark : .light
+                HStack {
+                    Toggle("Сменить тему", isOn: $themeManager.isDarkMode)
+                        .toggleStyle(CustomToggleStyle())  // custom
+                        .onChange(of: themeManager.isDarkMode) { newValue in
+                            themeManager.updateUITheme() // update interface
                         }
-                    }
-                    .background(Color(.systemBackground)) // Меняет цвет в зависимости от темы
-                    
+                    Spacer() // Отступ слева от текста, чтобы он не сдвигался
+                }
                 
                 Divider()
                 
@@ -63,8 +64,42 @@ struct ProfileSummary: View {
         }
     }
 }
+                
+                
+// MARK: custom toggle struct
+
+struct CustomToggleStyle: ToggleStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.label
+                .foregroundColor(.primary)
+            Spacer()
+            
+            // toggle
+            RoundedRectangle(cornerRadius: 16)
+                .strokeBorder(Color.primary, lineWidth: 2)
+                .background(
+                    RoundedRectangle(cornerRadius: 16)
+                        .fill(configuration.isOn ? Color.gray : Color.black)
+                )
+                .frame(width: 50, height: 30)
+                .overlay(
+                    Circle()
+                        .foregroundColor(.white)
+                        .padding(3)
+                        .offset(x: configuration.isOn ? 13 : -13)
+                )
+                .onTapGesture {
+                    configuration.isOn.toggle()
+                }
+        }
+    }
+}
+// MARK: END (custom toggle struct)
+
 
 #Preview {
     ProfileSummary(profile: Profile.default)
         .environment(ModelData())
+        .environmentObject(ThemeManager())
 }
